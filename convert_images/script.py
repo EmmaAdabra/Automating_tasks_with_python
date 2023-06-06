fab09d03647f624d82e1da0beb74ae5418d98d8a
EmmaAdabra <adabraemma@gmail.com>
2023-06-06 01:29:52 +0100

    Add get_img_path() And empty main()
    get_img_path(): gets path to the image dir as paramter, creat an
    address path to the images, add them to a list and return the list

--- convert_images/script.py
+++ convert_images/script.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
+
 from PIL import Image
+import unittest
+import os
+
+home = os.path.expanduser("~")
+file_path = os.path.join(home, "images")
+save_path = "ls /opt/icons"
 
 
 def rotate_img():
     pass
+
+
+def get_img_path(file_path):
+    """create a list of address path to the images"""
+    image_paths = [
+        os.path.join(file_path, img_name) for img_name in os.listdir(file_path)
+    ]
+    print(image_paths)
+    return image_paths
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
