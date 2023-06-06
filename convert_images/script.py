7acb25692b41625e0a0be30bc438a55aa74ff0cb
EmmaAdabra <adabraemma@gmail.com>
2023-06-06 02:37:28 +0100

    Add process_img(), Change vairable name
    process_img(): this function get an image object as arg
    rotate the image 90 degree clockwise and change the mode
    to RGB.
    file_path global variable change to "dir_path" for better
    clearity

--- convert_images/script.py
+++ convert_images/script.py
@@ -5,26 +5,35 @@ import unittest
 import os
 
 home = os.path.expanduser("~")
-file_path = os.path.join(home, "images")
+dir_path = os.path.join(home, "images")
 save_path = "ls /opt/icons"
 
-
-def rotate_img():
-    pass
-
-
-def get_img_path(file_path):
+def get_img_path(dir_path):
     """create a list of address path to the images"""
-    image_paths = [
-        os.path.join(file_path, img_name) for img_name in os.listdir(file_path)
-    ]
-    print(image_paths)
+    image_paths = [os.path.join(dir_path, img_name) for img_name in os.listdir(dir_path)]
     return image_paths
+   
+
+def process_img(img):
+    """convert image to RGB mode and rotate it 90 degree clockwise"""
+    img = img.convert("RGB")
+    return img.rotate(90)
 
 
 def main():
+    # count = 0
+    # if not os.path.exists(dir_path):
+        # print("Directory does not exist")
+    # if len(os.listdir(dir_path)) == 0:
+        # print("Directory is empty")
+    # 
+    # for img in get_img_path(dir_path):
+        # img = Image.open(img)
+        # img = process_img(img)
+        # print(img.mode)
+        # if count == 5:
+            # break
+        # count += 1
     pass
-
-
 if __name__ == "__main__":
     main()
