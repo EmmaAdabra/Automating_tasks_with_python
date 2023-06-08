8bc731f39438278236764125e1fafa5d81beb236
EmmaAdabra <adabraemma@gmail.com>
2023-06-08 09:37:23 +0100

    Added comments to aid readability and maintainability

--- convert_images/script.py
+++ convert_images/script.py
@@ -3,13 +3,13 @@
 from PIL import Image
 import os
 
-home = os.path.expanduser("~")
-dir_path = os.path.join(home, "images")
-save_path = os.path.join(home, "icons")
+home = os.path.expanduser("~")  # get full path to user dir
+dir_path = os.path.join(home, "images")  # create full path to images source dir
+save_path = os.path.join(home, "icons")  # create full path to dest (save) dir
 
 
 def get_images(dir_path):
-    """create a list of address path to the images"""
+    """create a list of images to be converted"""
     images = [
         os.path.join(dir_path, img_name)
         for img_name in os.listdir(dir_path)
@@ -20,25 +20,28 @@ def get_images(dir_path):
 
 def process_img(img):
     """convert image to RGB mode, resize image to 128x128 and rotate it -90 degree clockwise"""
-    img = img.convert("RGB")
-    img = img.resize((128, 128))
-    return img.rotate(-90)
+    img = img.convert("RGB")  # change image mode to RGB
+    img = img.resize((128, 128))  # resize image to 128 x 128
+    return img.rotate(-90)  # rotate return image to 90 deg clockwise direction
 
 
 def save_as_jpeg(img_name, img):
     """save image to a new directory in JPEG format"""
+    # check if destination dir exists and create one if not
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    output_path = os.path.join(save_path, img_name + ".jpeg")
-    img.save(output_path)
+    img.save(os.path.join(save_path, img_name + ".jpeg"))  # save image in format
 
 
 def main():
+    # check if source directory exist
     if not os.path.exists(dir_path):
         print("Directory does not exist")
+    # check if source directory is empty
     if len(os.listdir(dir_path)) == 0:
         print("Directory is empty")
 
+    # pass each image obj to the appropriate function
     for image in get_images(dir_path):
         img_name = os.path.basename(image)
         img = Image.open(image)
@@ -46,5 +49,6 @@ def main():
         save_as_jpeg(img_name, img)
 
 
+# only run script as main module
 if __name__ == "__main__":
     main()
