d604744b73454699e43f9f4794ad3cb79bed2643
EmmaAdabra <adabraemma@gmail.com>
2023-06-08 23:04:34 +0100

    rename directory, update run.py script
    serialization/feedback rename to serialization/users_feedback
    for clarity.
    run.py updated to create path to users_feedback directory

--- serialization/run.py
+++ serialization/run.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# import the required module
 import os
 import json
 import requests
+
+# create path to users_feedback files
+project_path = os.path.join(os.path.expanduser("~"), os.path.join
+("automate_real_world_tasks", "serialization"))
+feedback_txt_path = os.path.join(project_path, "users_feedback")
+
+print(feedback_txt_path)
