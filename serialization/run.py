deab2bd9bf5ac6ec4c13c4586580e72b423aa58b
EmmaAdabra <adabraemma@gmail.com>
2023-06-09 00:53:36 +0100

    Add parse_feedback_file(), rename variable, add test file, add comment

--- serialization/run.py
+++ serialization/run.py
@@ -5,16 +5,44 @@ import os
 import json
 import requests
 
-# create path to users_feedback files
+# create path to the project directory
 project_path = os.path.join(
     os.path.expanduser("~"), os.path.join("automate_real_world_tasks", "serialization")
 )
-feedback_file_path = os.path.join(project_path, "users_feedback")
+# create path to the directory containing  user's feedback
+feedback_files_dir = os.path.join(project_path, "users_feedback")
 
 
-def get_users_feedback(file_path):
+def get_users_feedback(file_dir_path):
     """get path to user's feedback directory as parameter, return a list of all feedbacks in the dir"""
-    users_feedback = [
-        os.path.join(file_path, txt_file) for txt_file in os.listdir(file_path)
+    # list of users feedback
+    feedback_file_list = [
+        os.path.join(file_dir_path, txt_file) for txt_file in os.listdir(file_dir_path)
     ]  # create a list of users feedback
-    return users_feedback
+    return feedback_file_list
+
+
+def parse_feedback_file(feedback_file_list):
+    """get list of user feedbacks file path, open the text file, parse the feedback txt to dictionary and return a list users feedback dictionary"""
+    feedback_dict = []  # holds the user's feedback dictionary
+
+    # iterate through feedback file in the list
+    for feedback in feedback_file_list:
+        add_feedback = (
+            {}
+        )  # tmp dictionary use to add user's feedback dictionary to the list
+
+        # open each feedback file in txt format
+        with open(feedback, mode="r", newline="") as feedback_txt:
+            # add the content of the feedback file to a list
+            data = [row.strip() for row in feedback_txt.readlines()]
+        # create a key value pair (dictionary) for each content of the feedback file
+
+        (
+            add_feedback["title"],
+            add_feedback["name"],
+            add_feedback["date"],
+            add_feedback["feedback"],
+        ) = data
+        feedback_dict.append(add_feedback)  # append the dictionary to a list
+    return feedback_dict
