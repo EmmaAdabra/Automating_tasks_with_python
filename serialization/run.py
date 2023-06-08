764bfbdb31a56706325d695aba2827ebcbf72bbb
EmmaAdabra <adabraemma@gmail.com>
2023-06-08 23:39:57 +0100

    Add get_users_feedback() to run.py, add user feedback file
    add get_users_feedback() to run.py that accesses
    feedback_file_path add returns a list containing all the files
    a new feedback file added to users_feedback/

--- serialization/run.py
+++ serialization/run.py
@@ -6,8 +6,15 @@ import json
 import requests
 
 # create path to users_feedback files
-project_path = os.path.join(os.path.expanduser("~"), os.path.join
-("automate_real_world_tasks", "serialization"))
-feedback_txt_path = os.path.join(project_path, "users_feedback")
+project_path = os.path.join(
+    os.path.expanduser("~"), os.path.join("automate_real_world_tasks", "serialization")
+)
+feedback_file_path = os.path.join(project_path, "users_feedback")
 
-print(feedback_txt_path)
+
+def get_users_feedback(file_path):
+    """get path to user's feedback directory as parameter, return a list of all feedbacks in the dir"""
+    users_feedback = [
+        os.path.join(file_path, txt_file) for txt_file in os.listdir(file_path)
+    ]  # create a list of users feedback
+    return users_feedback
